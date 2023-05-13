2bc6c033b8d2f7e396f679f8bd415addcd358afb
k-wakamatsu-tms <69659176+k-wakamatsu-tms@users.noreply.github.com>
2023-05-13 21:04:29 +0900

    first

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,29 @@ module.exports = {
     'next/core-web-vitals',
     'google',
     'prettier',
+    'plugin:storybook/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+    },
   ],
   rules: {
     'require-jsdoc': ['off'],
-    'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
     '@next/next/no-img-element': ['off'],
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['css'],
+      },
+    ],
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,26 @@
+'use client';
 import styles from './page.module.scss';
 import { Card } from '@/components/Card';
+import React, { use, useState } from 'react';
 
-export default function Home() {
+export default function Form() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.pageTitle}>
-          <a href="https://nextjs.org">Next.js</a> latest starter
-        </h1>
-
-        <p className={styles.message}>
-          Get started by editing <code>app/page.tsx</code>
-        </p>
-
-        <div className={styles.cards}>
-          <Card
-            href="https://nextjs.org/docs"
-            title="Documentation"
-            description="Find in-depth information about Next.js features and API."
-          />
-
-          <Card
-            href="https://nextjs.org/learn"
-            title="Learn"
-            description="Learn about Next.js in an interactive course with quizzes!"
-          />
-
-          <Card
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            title="Examples"
-            description="Discover and deploy boilerplate example Next.js projects."
-          />
-
-          <Card
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Deploy"
-            description="Instantly deploy your Next.js site to a public URL with Vercel."
-          />
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/tsuki-lab"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          by hanetsuki
-        </a>
-      </footer>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        名前：
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <button type="submit">確認</button>
+    </form>
   );
 }
